4e4d1bad54e73a53ec36d9ef663b388c42a4fc98
Kenneth C <76769232+Munckenh@users.noreply.github.com>
2024-02-05 11:21:59 +0800

    Create `bubbleSort.c`

---
+++ teacher-activities/bubbleSort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void swap(int* a, int* b);
+void bubble_sort(int* array, int size);
+void print_array(int* array, int size);
+
+int main() {
+    int unsorted_array[] = {4, 5, 6, 3, 1, 2, 9, 7, 8};
+    bubble_sort(unsorted_array, sizeof(unsorted_array)/sizeof(unsorted_array[0]));
+    print_array(unsorted_array, sizeof(unsorted_array)/sizeof(unsorted_array[0]));
+    return 0;
+}
+
+void swap(int* a, int* b) {
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+}
+
+void bubble_sort(int* array, int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (array[j] > array[j+1]) {
+                swap(&array[j], &array[j+1]);
+            }
+        }
+    }
+}
+
+void print_array(int* array, int size) {
+    for (int i = 0; i < size; ++i) {
+        if (i == 0) {
+            printf("{%d", array[i]);
+        } else {
+            printf(", %d", array[i]);
+        }
+    }
+    printf("}");
+}
