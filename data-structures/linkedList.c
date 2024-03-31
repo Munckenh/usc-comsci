3d1a02a4659ff510d2a9a28a99ae6fa70b23548f
Munckenh <76769232+Munckenh@users.noreply.github.com>
2024-04-01 03:14:57 +0800

    Convert `linkedList.c` into a custom header file

--- data-structures/linkedList.c
+++ data-structures/linkedList.c
@@ -1,28 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct node {
-    int data;
-    struct node* next;
-};
-
-void display(struct node* head);
-void insert_at_beginning(struct node* head, int data);
-void insert_at_index(struct node* head, int data, int index);
-void insert_at_end(struct node* head, int data);
-void delete_by_index(struct node* head, int index);
-void delete_by_data(struct node* head, int data);
-void insert_sorted(struct node* head, int data);
-
-int main() {
-    struct node* head = (struct node*)malloc(sizeof(struct node));
-    head->next = NULL;
-    insert_at_end(head, 10);
-    insert_at_end(head, 20);
-    insert_at_end(head, 30);
-    insert_at_end(head, 40);
-    display(head);
-}
+#include "linkedlist.h"
 
 void display(struct node* head) {
     struct node* current = head;
