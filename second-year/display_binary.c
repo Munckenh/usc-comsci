0046f371c9b21d790ec96722258a23f8de957cd0
Munckenh <76769232+Munckenh@users.noreply.github.com>
2024-09-11 09:42:25 +0800

    Avoid ternary operators, use assignment operators

--- second-year/display_binary.c
+++ second-year/display_binary.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 void display_binary(unsigned int value) {
-    for (unsigned int mask = 1 << (8 * sizeof(unsigned int) - 1); mask > 0; mask = mask >> 1) {
-        printf("%d", (mask & value) ? 1 : 0);
+    for (unsigned int mask = 1 << (8 * sizeof(unsigned int) - 1); mask > 0; mask >>= 1) {
+        printf("%d", (mask & value) != 0);
     }
     printf("\n");
 }
